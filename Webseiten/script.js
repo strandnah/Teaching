b545eea0a42b36d10a7c7ca822ab75230291e1c4
jhorstmann <jan.mathis.horsmann@iee.fraunhofer.de>
2025-01-31 07:57:11 +0100

    just formatted the code

--- Webseiten/script.js
+++ Webseiten/script.js
@@ -1,12 +1,12 @@
-document.querySelectorAll('.accordion-button').forEach(button => {
-  button.addEventListener('click', () => {
-    const id = button.id.split('-')[0] + '-info';
+document.querySelectorAll(".accordion-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const id = button.id.split("-")[0] + "-info";
     const content = document.getElementById(id);
-    document.querySelectorAll('.accordion-content').forEach(item => {
+    document.querySelectorAll(".accordion-content").forEach((item) => {
       if (item !== content) {
-        item.classList.add('hidden');
+        item.classList.add("hidden");
       }
     });
-    content.classList.toggle('hidden');
+    content.classList.toggle("hidden");
   });
 });
